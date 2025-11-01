08aad84adbb200acb0a0b45fcf45b072bb4e0637
Poussy Ayman <poussy.ayman1@gmail.com>
2025-11-01 14:21:26 +0200

    Update Header.jsx

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -58,6 +58,7 @@ const Header = ({ sidebarOpen, toggleSidebar }) => {
           <Link to="/">Home</Link>
           <Link to="/tournaments">Tournaments</Link>
           <Link to="/about">About Us</Link>
+          <Link to="/clubroles">Join Us</Link>
           {/* <Link to="/contact">Contact</Link>
           <Link to="/signup">
             <button className="signup-btn">Sign Up</button>
