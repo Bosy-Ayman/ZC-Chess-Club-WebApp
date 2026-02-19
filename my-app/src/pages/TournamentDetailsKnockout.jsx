7de5acc572970e5ec1eb2b4db07e37158e27023d
Poussy Ayman <poussy.ayman1@gmail.com>
2026-02-19 22:32:18 +0200

    Update TournamentDetailsKnockout.jsx

--- my-app/src/pages/TournamentDetailsKnockout.jsx
+++ my-app/src/pages/TournamentDetailsKnockout.jsx
@@ -17,16 +17,7 @@ export default function TournamentDetailsKnockout() {
             <h1>1st Knockout Tournament - Fall 2025</h1><br />
           </div>
 
-          <div className="iframe-container">
-            <iframe
-              src="https://challonge.com/74k1rd7u/module?theme=2&background_color=1a1a1a&font_color=ffffff"
-              width="100%"
-              height="600"
-              frameBorder="0"
-              scrolling="auto"
-              allowTransparency="true"
-            ></iframe>
-          </div>
+  
         </div>
 
         <Footer />
